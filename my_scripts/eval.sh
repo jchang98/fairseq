73ea37a03cbca8d818c7013c07278de731a8551e
standby <43020016+jchang98@users.noreply.github.com>
2023-11-12 19:10:26 +0800

    Update eval.sh

--- my_scripts/eval.sh
+++ my_scripts/eval.sh
@@ -1,5 +1,5 @@
-base_dir=/SISDC_GPFS/Home_SE/xyduan-suda/cjin/fairseq
-save=/SISDC_GPFS/Home_SE/xyduan-suda/cjin/fairseq_save
+base_dir=xxx/fairseq
+save=xxx/fairseq_save
 task_name=ldc-zhen-baseline
 avg_epoch=0
 
